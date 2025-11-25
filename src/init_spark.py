1311c48fd016c0e581ef1c449b6e4d8db65e00d7
ValentinRapp <valentin.rapp@epitech.eu>
2025-11-25 16:42:37 +0100

    increased memory limit for spark

--- src/init_spark.py
+++ src/init_spark.py
@@ -3,6 +3,9 @@ from pyspark.sql import SparkSession
 def init():
     spark = SparkSession.builder \
         .appName("DisinfoBotWatch") \
+        .config("spark.driver.memory", "4g") \
+        .config("spark.executor.memory", "4g") \
+        .config("spark.sql.shuffle.partitions", "100") \
         .getOrCreate()
 
     spark.sparkContext.setLogLevel("ERROR")
